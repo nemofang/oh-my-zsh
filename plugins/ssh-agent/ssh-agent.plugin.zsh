41b8b42336263da1dcc55e746821942b25f82387
Nemo Fang <nfang@techninja.pro>
2025-09-02 16:24:45 +0300

    Update plugins to latest.

--- plugins/ssh-agent/ssh-agent.plugin.zsh
+++ plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -6,9 +6,16 @@ function _start_agent() {
   if [[ -f "$ssh_env_cache" ]]; then
     . "$ssh_env_cache" > /dev/null
 
-    {
-      [[ "$USERNAME" = root ]] && command ps ax || command ps x
-    } | command grep ssh-agent | command grep -q $SSH_AGENT_PID && return 0
+    # Test if $SSH_AUTH_SOCK is visible
+    zmodload zsh/net/socket
+    if [[ -S "$SSH_AUTH_SOCK" ]] && zsocket "$SSH_AUTH_SOCK" 2>/dev/null; then
+      return 0
+    fi
+  fi
+
+  if [[ ! -d "$HOME/.ssh" ]]; then
+    echo "[oh-my-zsh] ssh-agent plugin requires ~/.ssh directory"
+    return 1
   fi
 
   # Set a maximum lifetime for identities added to ssh-agent
@@ -16,7 +23,7 @@ function _start_agent() {
   zstyle -s :omz:plugins:ssh-agent lifetime lifetime
 
   # start ssh-agent and setup environment
-  echo Starting ssh-agent...
+  zstyle -t :omz:plugins:ssh-agent quiet || echo >&2 "Starting ssh-agent ..."
   ssh-agent -s ${lifetime:+-t} ${lifetime} | sed '/^echo/d' >! "$ssh_env_cache"
   chmod 600 "$ssh_env_cache"
   . "$ssh_env_cache" > /dev/null
@@ -36,7 +43,7 @@ function _add_identities() {
   # this is to mimic the call to ssh-add with no identities
   if [[ ${#identities} -eq 0 ]]; then
     # key list found on `ssh-add` man page's DESCRIPTION section
-    for id in id_rsa id_dsa id_ecdsa id_ed25519 identity; do
+    for id in id_rsa id_dsa id_ecdsa id_ed25519 id_ed25519_sk identity; do
       # check if file exists
       [[ -f "$HOME/.ssh/$id" ]] && identities+=($id)
     done
@@ -55,7 +62,7 @@ function _add_identities() {
     # if id is an absolute path, make file equal to id
     [[ "$id" = /* ]] && file="$id" || file="$HOME/.ssh/$id"
     # check for filename match, otherwise try for signature match
-    if [[ ${loaded_ids[(I)$file]} -le 0 ]]; then
+    if [[ -f $file && ${loaded_ids[(I)$file]} -le 0 ]]; then
       sig="$(ssh-keygen -lf "$file" | awk '{print $2}')"
       [[ ${loaded_sigs[(I)$sig]} -le 0 ]] && not_loaded+=("$file")
     fi
@@ -70,13 +77,16 @@ function _add_identities() {
   local args
   zstyle -a :omz:plugins:ssh-agent ssh-add-args args
 
+  # if ssh-agent quiet mode, pass -q to ssh-add
+  zstyle -t :omz:plugins:ssh-agent quiet && args=(-q $args)
+
   # use user specified helper to ask for password (ksshaskpass, etc)
   local helper
   zstyle -s :omz:plugins:ssh-agent helper helper
 
   if [[ -n "$helper" ]]; then
     if [[ -z "${commands[$helper]}" ]]; then
-      echo "ssh-agent: the helper '$helper' has not been found."
+      echo >&2 "ssh-agent: the helper '$helper' has not been found."
     else
       SSH_ASKPASS="$helper" ssh-add "${args[@]}" ${^not_loaded} < /dev/null
       return $?
@@ -86,34 +96,24 @@ function _add_identities() {
   ssh-add "${args[@]}" ${^not_loaded}
 }
 
-# test if agent-forwarding is enabled
-zstyle -b :omz:plugins:ssh-agent agent-forwarding agent_forwarding
-
-# Add a nifty symlink for screen/tmux if agent forwarding
-if [[ $agent_forwarding = "yes" && -n "$SSH_AUTH_SOCK" && ! -L "$SSH_AUTH_SOCK" ]]; then
-  ln -sf "$SSH_AUTH_SOCK" /tmp/ssh-agent-$USERNAME-screen
+# Add a nifty symlink for screen/tmux if agent forwarding is enabled
+if zstyle -t :omz:plugins:ssh-agent agent-forwarding \
+   && [[ -n "$SSH_AUTH_SOCK" ]]; then
+  if [[ ! -L "$SSH_AUTH_SOCK" ]]; then
+    if [[ -n "$TERMUX_VERSION" ]]; then
+      ln -sf "$SSH_AUTH_SOCK" "$PREFIX"/tmp/ssh-agent-$USERNAME-screen
+    else
+      ln -sf "$SSH_AUTH_SOCK" /tmp/ssh-agent-$USERNAME-screen
+    fi
+  fi
 else
   _start_agent
 fi
 
-() {
-  emulate -L zsh
-
-  command mkdir "$ZSH_CACHE_DIR/ssh-agent.lock" 2>/dev/null || return
-
-  trap "
-    ret=\$?
-
-    command rm -rf '$ZSH_CACHE_DIR/ssh-agent.lock'
-    unset agent_forwarding ssh_env_cache
-    unfunction _start_agent _add_identities 2>/dev/null
-
-    return \$ret
-  " EXIT INT QUIT
-
+# Don't add identities if lazy-loading is enabled
+if ! zstyle -t :omz:plugins:ssh-agent lazy; then
   _add_identities
-
-}
+fi
 
 unset agent_forwarding ssh_env_cache
 unfunction _start_agent _add_identities
